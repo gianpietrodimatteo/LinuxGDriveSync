21e41ae1cc13b2cd660b5bd28b12f531db4550de
Gianpietro Di Matteo <gianpietro.dimatteo@gmail.com>
2023-08-31 16:25:08 -0300

    Tested installation script

--- install.sh
+++ install.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-# LinuxGDrive sync installer
+# LinuxGDriveSync installer
 # A Google Drive syncing tool for Linux based systems using Rclone
-# Requires sudo, rclone and systemd
+# Requires rclone and systemd
+
+rmCp() {
+    rm -f "$2"
+    cp "$1" "$2"
+}
+
+echo "Installing LinuxGDriveSync"
 
 # Creates directories
 mkdir -p ~/.config/rclone ~/bin ~/.config/systemd/user ~/GoogleDrive
@@ -20,17 +27,16 @@ chmod +x "$(pwd)/rclone-login.sh"
 chmod +x "$(pwd)/rclone-sync.sh"
 
 # Install scripts
-ln -sv "$(pwd)/rclone-login.sh" ~/bin/
-ln -sv "$(pwd)/rclone-sync.sh" ~/bin/
+rmCp "$(pwd)/rclone-login.sh" ~/bin/rclone-login.sh
+rmCp "$(pwd)/rclone-sync.sh" ~/bin/rclone-sync.sh
 
 # Install services and timer
-ln -sv "$(pwd)/rclone-logout.service" ~/.config/systemd/user/
-ln -sv "$(pwd)/rclone-login.service" ~/.config/systemd/user/
-ln -sv "$(pwd)/rclone-sync.service" ~/.config/systemd/user/
-ln -sv "$(pwd)/rclone-sync.timer" ~/.config/systemd/user/
+rmCp "$(pwd)/rclone-logout.service" ~/.config/systemd/user/rclone-logout.service
+rmCp "$(pwd)/rclone-login.service" ~/.config/systemd/user/rclone-login.service
+rmCp "$(pwd)/rclone-sync.service" ~/.config/systemd/user/rclone-sync.service
+rmCp "$(pwd)/rclone-sync.timer" ~/.config/systemd/user/rclone-sync.timer
 
 # Enable and start services and timer
-sudo systemctl enable --now rclone-sync.timer
-sudo systemctl enable --now rclone-logout.service
-sudo systemctl enable --now rclone-login.service
-sudo systemctl daemon-reload
+systemctl --user enable --now rclone-sync.timer
+systemctl --user enable --now rclone-logout.service
+systemctl --user enable --now rclone-login.service
