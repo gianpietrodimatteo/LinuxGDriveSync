51ec57bce539b1cd5b8c1bd64b455ad522f89138
Gianpietro Di Matteo <gianpietro.dimatteo@gmail.com>
2023-08-31 15:05:47 -0300

    Initial commit Starting shellcheck adjustments

---
+++ install.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# LinuxGDrive sync installer
+# A Google Drive syncing tool for Linux based systems using Rclone
+# Requires sudo, rclone and systemd
+
+# Creates directories
+mkdir -p ~/.config/rclone ~/bin ~/.config/systemd/user ~/GoogleDrive
+
+# Configure rclone access tokens
+echo "Read https://rclone.org/drive/"
+echo "Name remote GoogleDrive"
+rclone config
+
+# Make initial sync
+rclone sync -v GoogleDrive: ~/GoogleDrive
+
+# Install scripts
+ln -sv "$(pwd)/rclone-login.sh" ~/bin/
+ln -sv "$(pwd)/rclone-sync.sh" ~/bin/
+
+# Install services and timer
+ln -sv "$(pwd)/rclone-logout.service" ~/.config/systemd/user/
+ln -sv "$(pwd)/rclone-login.service" ~/.config/systemd/user/
+ln -sv "$(pwd)/rclone-sync.service" ~/.config/systemd/user/
+ln -sv "$(pwd)/rclone-sync.timer" ~/.config/systemd/user/
+
+# Enable and start services and timer
+sudo systemctl enable --now rclone-sync.timer
+sudo systemctl enable --now rclone-logout.service
+sudo systemctl enable --now rclone-login.service
+sudo systemctl daemon-reload
---
+++ rclone-login.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Rclone login script
+FLAGPATH=$HOME/.config/rclone
+DRIVEPATH=$HOME/GoogleDrive
+
+# Check if the flag file from the last session exists; if it does, don't proceed with sync
+if [ -e "$FLAGPATH"/last_session_sync_failed ]; then
+  echo "Last session sync failed. Skipping the startup sync."
+  touch "$FLAGPATH"/no_sync_flag  # Create a flag file to signal cron job not to run
+  exit 0
+else
+  echo "Last session sync was successful."
+  rm -f "$FLAGPATH"/no_sync_flag  # Remove the flag file to allow the cron job to run
+fi
+
+# Perform the initial sync from Google Drive to local machine
+rclone sync -v GoogleDrive: "$DRIVEPATH" > "$FLAGPATH"/login-log.log 2>&1
+
+# Check if rclone encountered any errors during the sync
+if [ $? -ne 0 ]; then
+  echo "Error during initial sync from Google Drive. Please check and resolve the issue."
+  touch $FLAGPATH/no_sync_flag  # Create a flag file to signal cron job not to run
+else
+  echo "Initial sync successful."
+  rm -f $FLAGPATH/no_sync_flag  # Remove the flag file if sync is successful
+fi
+
+
+if ! rclone sync -v GoogleDrive: "$DRIVEPATH" > "$FLAGPATH"/login-log.log 2>&1;
+then
+  echo "Error during initial sync from Google Drive. Please check and resolve the issue."
+  touch $FLAGPATH/no_sync_flag  # Create a flag file to signal cron job not to run
+else
+  echo "Initial sync successful."
+  rm -f $FLAGPATH/no_sync_flag  # Remove the flag file if sync is successful
+fi
